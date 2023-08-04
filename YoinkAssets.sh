3e58bcdbefbd527d3329575c1f1bae290ce5166b
OtterDev <134523836+OtterCodes101@users.noreply.github.com>
2023-08-04 20:32:23 +0000

    Add YoinkAssets

---
+++ YoinkAssets.sh
@@ -0,0 +1,6 @@
+# fun fact: this is perfectly legal
+echo "Yoinking assets.."
+wget -O mcp918/1.8.8.jar https://launcher.mojang.com/mc/game/1.8.8/client/0983f08be6a4e624f5d85689d1aca869ed99c738/client.jar
+wget -O mcp918/1.8.json https://launchermeta.mojang.com/mc/assets/1.8/e264980ad255aad2174cbe4d674c102474ae5202/1.8.json
+wget -O mcp918/mcp918.zip http://www.modcoderpack.com/files/mcp918.zip
+echo "Done!"
